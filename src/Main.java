f73d691fddd9bd7f4b8a5c6581786720376c79f7
Grigoriy <lakiza.grigoriy@gmail.com>
2024-01-11 01:50:07 +0200

    Draft for homework

--- src/Main.java
+++ src/Main.java
@@ -7,72 +7,125 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
+import java.time.LocalDateTime;
 
 public class Main {
     private static final String FILE_NAME = "resources/smartphones100.csv";
 
     static String[] readFileWithBufferReader(String filename) {
         ArrayList<String> data = new ArrayList<>();
-        FileReader reader;
 
-        try {
-            reader = new FileReader(filename);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        var br = new BufferedReader(reader);
-        String newLine;
-
-        try {
+        try (FileReader reader = new FileReader(filename);
+             BufferedReader br = new BufferedReader(reader)) {
+            String newLine;
             while ((newLine = br.readLine()) != null) {
                 data.add(newLine);
             }
-            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return Arrays.copyOf(data.toArray(), data.size(), String[].class);
     }
 
+    private static List<Smartphone> createSmartphoneObject(String[] data) {
+        List<Smartphone> smartphones = new ArrayList<>();
+        phonesListObject(data, smartphones);
+        return smartphones;
+    }
+
+    public static void phonesListObject(String[] data, List<Smartphone> smartphones) {
+        for (int c = 1; c < data.length; c++) {
+            var pieces = data[c].split(",");
+            Smartphone currentSmartphone = createSmartphone(pieces);
+            smartphones.add(currentSmartphone);
+        }
+    }
+
+    public static Smartphone createSmartphone(String[] pieces) {
+        Smartphone currentSmartphone = null;
+        switch (pieces[0].toLowerCase()) {
+            case "apple":
+                currentSmartphone = new Apple(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                        Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
+                        Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]),
+                        Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
+                break;
+            case "samsung":
+                currentSmartphone = new Samsung(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                        Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
+                        Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]),
+                        Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
+                break;
+            case "huawei":
+                currentSmartphone = new Huawei(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                        Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
+                        Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]), Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
+                break;
+            case "xiaomi":
+                currentSmartphone = new Xiaomi(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                        Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
+                        Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]), Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
+                break;
+        }
+        return currentSmartphone;
+    }
+
     public static void main(String[] args) {
         long timerStart = System.currentTimeMillis();
-        Set<Smartphone> samsungs = new LinkedHashSet<>();
+        Set<Smartphone> currentSmartphone = new LinkedHashSet<>();
         long importFileTimerStart = System.currentTimeMillis();
         String[] data = readFileWithBufferReader(FILE_NAME);
-        long importFileTimerFinish = System.currentTimeMillis() - importFileTimerStart;
-        long objectCreationTimerStart = System.currentTimeMillis();
-        phonesListObject(data);
-        long objectCreationTimerFinish = System.currentTimeMillis() - objectCreationTimerStart;
-        Stream<Smartphone> stream = samsungs.stream();
+        long importFileTimerEnd = System.currentTimeMillis();
+        long createObjectTimerStart = System.currentTimeMillis();
+        List<Smartphone> smartphones = createSmartphoneObject(data);
+        long createObjectTimerEnd = System.currentTimeMillis();
 
-        Stream<Smartphone> sortedStream = samsungs.stream()
-                .sorted((s1, s2) -> Integer.compare(s2.getYearReleased(), s1.getBuildInMemoryStorage()));
+        Stream<Smartphone> smartphoneStream1 = smartphones.stream();
+        Stream<Smartphone> smartphoneStream2 = smartphones.stream();
+        Stream<Smartphone> smartphoneStream3 = smartphones.stream();
 
-        sortedStream.skip(16)
+        System.out.println("********************************");
+        System.out.println("Hryhorii Lakiza" + " / " + "Collection -> LinkedHashSet" + " / " + "SortOrder -> зменшення"
+                + " / " + "SortSkip->16" + " / " + "SortOut->24" + " / " + "FilterOut->21");
+        System.out.println("********************************");
+        System.out.println("1) Sorted Smartphones (by provided conditions):");
+        System.out.println("********************************");
+
+        smartphoneStream1
+                .sorted((s1, s2) -> Integer.compare(s2.getYearReleased(), s1.getBuildInMemoryStorage()))
+                .skip(16)
                 .limit(24)
                 .forEach(System.out::println);
 
-        stream
-                .sorted((s1, s2) -> Integer.compare(s2.getYearReleased(), s1.getBuildInMemoryStorage()))
-                .filter(s -> s.toString().toLowerCase().contains("samsung"))
+        System.out.println("********************************");
+        System.out.println("2) Filtered Smartphones (By Brand Name):");
+        System.out.println("********************************");
+        smartphoneStream2
+                .filter(s -> s.toString().contains("Apple"))
                 .limit(21)
                 .forEach(System.out::println);
 
+        System.out.println("********************************");
+        System.out.println("3) Mapped Smartphones (id + OS):");
+        System.out.println("********************************");
+        smartphoneStream3
+                .map(smartphone -> smartphone.getId() + " " + smartphone.getOperationSystem())
+                .forEach(System.out::println);
 
         var globalTimerFinish = System.currentTimeMillis() - timerStart;
         System.out.println("**********************************************");
-        System.out.println("File import time " + importFileTimerFinish + "ms");
-        System.out.println("Object creation time " + objectCreationTimerFinish + "ms");
+        System.out.println("Object creation time: " + (createObjectTimerEnd - createObjectTimerStart) + " ms");
+        System.out.println("Import file time: " + (importFileTimerEnd - importFileTimerStart) + " ms");
         System.out.println("All processes duration " + globalTimerFinish + "ms");
-
         System.out.println("**********************************************");
         LocalDateTime startDate = LocalDateTime.of(2023, 10, 22, 18, 00);
         LocalDateTime endDate = LocalDateTime.of(2024, 03, 07, 01, 00);
@@ -84,37 +137,5 @@ public class Main {
         System.out.println("Начальная дата:" + startDate);
         System.out.println("Конечная дата:" + endDate);
         System.out.println("Промежуток: " + days + " дней и " + hours + " часов");
-    }
-
-    public static void phonesListObject(String[] data) {
-        for (int c = 1; c < data.length; c++) {
-            var pieces = data[c].split(",");
-            Smartphone smartphones = null;
-            switch (pieces[0].toLowerCase()) {
-                case "apple":
-                    smartphones = new Apple(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
-                            Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]),
-                            Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
-                    break;
-                case "samsung":
-                    smartphones = new Samsung(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
-                            Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]),
-                            Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
-                    break;
-                case "huawei":
-                    smartphones = new Huawei(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
-                            Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]), Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
-                    break;
-                case "xiaomi":
-                    smartphones = new Xiaomi(pieces[0], pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Double.parseDouble(pieces[4]), Integer.parseInt(pieces[5]), pieces[6],
-                            Boolean.parseBoolean(pieces[7]), Integer.parseInt(pieces[8]), Integer.parseInt(pieces[9]), Integer.parseInt(pieces[10]));
-                    break;
-            }
-            System.out.println("Smartphone: " + smartphones);
-        }
     }
 }
